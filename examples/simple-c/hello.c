b277e01d8b1784137cb7648aa4da526cc8f533c1
Takashi Nakamoto <nyakamoto@gmail.com>
2020-04-04 17:33:56 -0700

    Revised the sample C program to show the arguments.

--- examples/simple-c/hello.c
+++ examples/simple-c/hello.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
-  printf("Hello world!\n");
+  printf("Hello world!!!\n");
+
+  if (argc >= 2) {
+    printf("First argument: %s\n", argv[1]);
+    if (argc >= 3) {
+      printf("Second argument: %s\n", argv[2]);
+    }
+  } else {
+    printf("No argument was specified\n");
+  }
 }
